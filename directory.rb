d9db9aef96d4e95b31796e5d9ae1f0198968acfa
Ormeline <ormelineluyengi@outlook.com>
2023-04-04 22:45:30 +0100

    exercise 5

--- directory.rb
+++ directory.rb
@@ -1,36 +1,36 @@
 # initialise a list of students' names
 students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-  ]
+  {name: "Dr. Hannibal Lecter", cohort: :November, nationality: "British", age: 21, height_cm: 160},
+  {name: "Darth Vader", cohort: :November, nationality: "German", age: 25, height_cm: 175}, 
+  {name: "Nurse Ratched", cohort: :November, nationality: "South African", age: 23, height_cm: 159},
+  {name: "Michael Corleone", cohort: :November, nationality: "Jamaican", age: 24, height_cm: 149},
+  {name: "Alex DeLarge", cohort: :November, nationality: "French", age: 25, height_cm: 161},
+  {name: "The Wicked Witch of the West", cohort: :November, nationality: "British", age: 23, height_cm: 151},
+  {name: "Terminator", cohort: :November, nationality: "French", age: 25, height_cm: 148},
+  {name: "Freddy Krueger", cohort: :November, nationality: "Congolese", age: 24, height_cm: 164},
+  {name: "The Joker", cohort: :November, nationality: "Nigerian", age: 23, height_cm: 174},
+  {name: "Joffrey Baratheon", cohort: :November, nationality: "British", age: 26, height_cm: 168},
+  {name: "Norman Bates", cohort: :November, nationality: "British", age: 24, height_cm: 178}
+]
 
-  # define a method to print the header of student list
-  def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
-  end 
+# define a method to print the header of student list
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end 
 
- # define a method to print the list of students' names and use the while loop
-  def print_students(students)
-    x = 0
-    while x < students.length
-        puts "#{x+1}. #{students[x][:name]} (#{students[x][:cohort]} cohort)"
-        x += 1
+# define a method to print the list of students' names and use the while loop
+def print_students(students)
+  x = 0
+  while x < students.length
+    puts "#{x+1}. #{students[x][:name]} (#{students[x][:cohort]} cohort). Nationality: #{students[x][:nationality]}. Age: #{students[x][:age]}. Height: #{students[x][:height_cm]}"
+    x += 1
   end 
 end 
 
 # define a method to print the footer of students' name list
 def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students"
 end 
 
 # call the methods to print the list of students' names
