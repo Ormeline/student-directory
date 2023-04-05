debfe97fe7d9a4c08f51ef3517ebf4219a0f7c44
Ormeline <ormelineluyengi@outlook.com>
2023-04-05 13:41:49 +0100

    exercise 8

--- directory.rb
+++ directory.rb
@@ -1,44 +1,70 @@
-# create an empty list of students and name
-students = []
-name = ""
+# create an empty hash to store the students grouped by cohort
+students_grouped_by_cohort = {}
 
-# use while loop until user enters 'done'
-while name.downcase != "done"
-  # prompt user to enter name 
-  puts "Please enter the name of a student (or enter 'done' if you're finished):"
+# use a loop until the user enters 'done'
+loop do
+  # prompt user to enter a name
+  print "Please enter the name of a student (or enter 'done' if you're finished): "
   name = gets.chomp
 
   # break loop if user enters done
-  if name.downcase == "done"
-    break
-  end
+  break if name.downcase == "done"
 
   # prompt user to enter cohort and convert user's input into a symbol
-  puts "Please enter the cohort of the student:"
-  cohort = gets.chomp.to_sym
+  print "Please enter the cohort of the student: "
+  cohort = gets.chomp.downcase.to_sym
 
-  # define the valid cohorts in a list of symbols 
-  valid_cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
-
-  #  program will loop until user enters valid cohort else it will print an error message
-  until valid_cohorts.include?(cohort)
-    puts "Invalid cohort entered. Please enter a valid cohort:"
-    cohort = gets.chomp.to_sym
+  # loop until user enters valid cohort else print an error message
+  until [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december].include?(cohort)
+    print "You have entered an invalid. Please enter a valid cohort: "
+    cohort = gets.chomp.downcase.to_sym
   end
 
-  # create a hash for the students and add it to the list
+  # create a hash for the student and add it to the list
   student = {
     name: name.empty? ? "Unknown" : name,
-    cohort: cohort.empty? ? :november : cohort.to_sym
+    cohort: cohort.empty? ? :november : cohort
   }
 
-  students << student
+  # group the students by their cohorts
+  if students_grouped_by_cohort[cohort].nil?
+    students_grouped_by_cohort[cohort] = [student]
+  else
+    students_grouped_by_cohort[cohort] << student
+  end
 end
 
-# go through the student list and print a student's name and cohort
-students.each_with_index do |student, index|
-  puts "#{index + 1}. #{student[:name]} (#{student[:cohort].capitalize} cohort)"
+# get a list of all existing cohorts
+cohorts = students_grouped_by_cohort.keys
+
+# prompt the user to select a cohort and display the names of the students in that cohort
+# iterate over the list of cohorts and ask user to select a cohort
+puts "Please enter a cohort you would like to see (#{cohorts.join(", ")}) or enter 'exit' to quit:"
+cohort_to_display = gets.chomp.downcase.to_sym
+
+# loop until user enters 'exit'
+until cohort_to_display == :exit
+  # check if the cohort entered exists and print the names of the students in that cohort
+  if students_grouped_by_cohort.key?(cohort_to_display)
+    puts "#{cohort_to_display.capitalize} Cohort:".center(50)
+    students_grouped_by_cohort[cohort_to_display].each_with_index do |student, index|
+      puts "#{index + 1}. #{student[:name]}"
+    end
+  else
+    puts "You have entered an invalid cohort. Please try again."
+  end
+
+  # ask user to enter another cohort or enter 'exit' to quit
+  puts "Please enter another cohort you would like to view (#{cohorts.join(", ")}) or enter 'exit' to quit:"
+  cohort_to_display = gets.chomp.downcase.to_sym
 end
+
+puts "Goodbye!"
+
+
+
+
+
 
 
 
