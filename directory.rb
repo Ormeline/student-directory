9a2e0df7f6a33b9719a8057a56db6b7968ad4dbb
Ormeline <ormelineluyengi@outlook.com>
2023-04-05 19:34:24 +0100

    step 10

--- directory.rb
+++ directory.rb
@@ -1,36 +1,23 @@
-def input_students(students_grouped_by_cohort)
+def input_students
   # prompt user to enter a name
-  print "Please enter the name of a student (or enter 'done' if you're finished): "
+  puts "Please enter the name of a student (or enter 'done' if you're finished): "
   name = gets.strip
 
-  # break loop if user enters done
-  return if name.downcase == "done"
+  # return loop if user enters done
+  if name.downcase == "done"
+    return
+  end
 
   # prompt user to enter cohort and convert user's input into a symbol
-  print "Please enter the cohort of the student: "
-  cohort = gets.strip.downcase.to_sym
+  puts "Please enter the cohort of the student: "
+  cohort = gets.strip.to_sym
 
-  # loop until user enters valid cohort else print an error message
-  until [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december].include?(cohort)
-    print "You have entered an invalid. Please enter a valid cohort: "
-    cohort = gets.strip.downcase.to_sym
-  end
-
-  # create a hash for the student and add it to the list
-  student = {
-    name: name.empty? ? "Unknown" : name,
-    cohort: cohort.empty? ? :november : cohort
-  }
-
-  # group the students by their cohorts
-  if students_grouped_by_cohort[cohort].nil?
-    students_grouped_by_cohort[cohort] = [student]
-  else
-    students_grouped_by_cohort[cohort] << student
-  end
-
-  # print a message indicating the number of students
-  total_students = students_grouped_by_cohort.values.flatten.length
+  # create a new student hash and add it to the appropriate cohort
+  @students ||= []
+  @students << { name: name, cohort: cohort }
+  
+  # get the total number of students and print message
+  total_students = @students.length
   if total_students == 1
     puts "Now we have 1 student."
   else
@@ -38,26 +25,22 @@ def input_students(students_grouped_by_cohort)
   end
 end
 
-def print_header()
-  puts "The students of my cohort at Makers Academy"
+def print_header
+  puts "The students of my cohort"
   puts "-------------"
 end
 
-def print_students(students_grouped_by_cohort)
-  students_grouped_by_cohort.each do |cohort, students|
-    puts "#{cohort.capitalize} Cohort:".center(20)
-    students.each_with_index do |student, index|
-      puts "#{index + 1}. #{student[:name]}"
-    end
-    puts ""
+def print_students_list
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students_grouped_by_cohort)
+def print_footer
   # get the total number of students
-  total_students = students_grouped_by_cohort.values.flatten.length
+  total_students = @students.length
 
-  # print a message indicating the number of students
+  # print a message indicating the number of students in either singular or pural
   if total_students == 1
     puts "Overall, we have 1 great student."
   else
@@ -65,40 +48,37 @@ def print_footer(students_grouped_by_cohort)
   end
 end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
 def interactive_menu
-  students_grouped_by_cohort = {}
+  @students = []
   loop do
-    # print menu options
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    # get user input
-    selection = gets.chomp
-
-    # perform action based on user input
-    case selection
-    when "1"
-      input_students(students_grouped_by_cohort)
-    when "2"
-      print_header
-      print_students(students_grouped_by_cohort)
-      print_footer(students_grouped_by_cohort)
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean. Please try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
 interactive_menu
-
-
-
-
-
-
-
-
-
